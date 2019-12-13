4ea83de2dc17b71b61c32a46159c1ff029e73cee
Leo <leo.zhou1986@hotmail.com>
2019-12-13 23:55:12 +1300

    Add side bar to ChatRoom

--- iMarket.CRM/ClientApp/src/components/ChatRoom.js
+++ iMarket.CRM/ClientApp/src/components/ChatRoom.js
@@ -9,19 +9,32 @@ import {
   Segment
 } from "semantic-ui-react";
 
+import Sidebar from "react-sidebar";
+
 export default class ChatRoom extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      sidebarOpen: true
     };
+    this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
   }
 
+  onSetSidebarOpen(open) {
+    this.setState({ sidebarOpen: open });
+  }
   render() {
     return (
-      <div>
-        <h1>ChatRoom</h1>
-        <p>This is a simple example of a React component.</p>
-      </div>
+      <Sidebar
+        sidebar={<b>Sidebar content</b>}
+        open={this.state.sidebarOpen}
+        onSetOpen={this.onSetSidebarOpen}
+        styles={{ sidebar: { background: "white" } }}
+      >
+        <button onClick={() => this.onSetSidebarOpen(true)}>
+          Open sidebar
+        </button>
+      </Sidebar>
     );
   }
 }

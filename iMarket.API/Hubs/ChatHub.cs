94c1c36ea7495852b4c632293b73d64ed1f966f5
Leo <leo.zhou1986@hotmail.com>
2019-12-30 23:15:31 +1300

    ChatHub add group functions

--- iMarket.API/Hubs/ChatHub.cs
+++ iMarket.API/Hubs/ChatHub.cs
@@ -23,5 +23,19 @@ namespace SignalRChat.Hubs
         {
             return Clients.User(user).SendAsync("ReceiveMessage", message);
         }
+
+        public async Task AddToGroup(string groupName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has joined the group {groupName}.");
+        }
+
+        public async Task RemoveFromGroup(string groupName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, groupName);
+
+            await Clients.Group(groupName).SendAsync("Send", $"{Context.ConnectionId} has left the group {groupName}.");
+        }
     }
 }
